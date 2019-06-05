26e8dd43c3461d6dc68be327b6444df40b78a4e6
evandrotvc <51249134+evandrotvc@users.noreply.github.com>
2019-06-05 16:01:30 -0300

    Atualizando funções camada física receptora

--- src/funcoes.cpp
+++ src/funcoes.cpp
@@ -48,3 +48,68 @@ void CamadaFisicaTransmissora(int* quadro){
     }
    // MeioDeComunicacao(fluxoBrutoDeBits);
 }
+
+// Camada fisica receptora
+void CamadaFisicaReceptora (int*  quadro) 
+{
+	int tipoDeDecodificacao = 0; //alterar de acordo o teste
+	int *fluxoBrutoDeBits ; //ATENÇÃO: trabalhar com BITS!!!
+	switch (tipoDeDecodificacao) 
+	{
+		case 0 : //codificao binaria
+		fluxoBrutoDeBits = CamadaFisicaReceptoraDecodificacaoBinaria(quadro);
+		break;
+		
+      case 1 : //codificacao manchester
+		//fluxoBrutoDeBits = CamadaFisicaReceptoraDecodificacaoManchester(quadro);
+		break;
+		
+      case 2 : //codificacao manchester diferencial
+		//fluxoBrutoDeBits = CamadaFisicaReceptoraDecodificacaoManchesterDiferencial(quadro);
+		break;
+
+	}//fim do switch/case
+	//chama proxima camada
+	//CamadaDeAplicacaoReceptora(fluxoBrutoDeBits);
+}//fim do metodo CamadaFisicaTransmissora
+
+int * CamadaFisicaReceptoraDecodificacaoBinaria (int *quadro ) 
+{
+	 //int num[4] = {6,3,2,1};
+   int quantidade = sizeof(quadro)/sizeof(int); // pega tamanho do vetor 
+   printf("quantidade %d\n",quantidade );
+   int i =0;
+   int aux,j=0;
+   int bytes = 7;
+ 
+ 	int *vetor_bin = (int *) malloc (quantidade * sizeof(int)) ; // aloca a quantidade de elementos do vetor
+
+    //Para converter de binario para decimal, basta decimal = bit7 * 2ˆ7 + bit6 * 2ˆ6 +... + bit0 * 2ˆ0
+   int base = 2,expoente = 0;
+   for(i = 0; i<quantidade; i++)
+   {
+      expoente = 0;
+      for (aux = bytes; aux >= 8*j ; aux--) // aux começa em bytes = 7, pois a conversao começa do bit menos significativo pro mais significativo.
+      {
+         vetor_bin[i] += quadro[aux] * pow(base,expoente); // conversao do binario para decimal
+         expoente++;
+      }
+      j++;
+      bytes += 8;
+      
+   }
+   int * msg_decodificada = vetor_bin;// Como o retorno é um ponteiro , necessita um ponteiro apontando para o vetor já decodificado
+   for (aux = 0; aux < quantidade; aux++) // for que serve apenas pra garatir que o valor armazenado seja o valor correto , usando o casting
+      msg_decodificada[aux] = uint16_t(vetor_bin[aux]);
+
+
+   // Printa mensagem decodificada
+  /* for (aux = 0; aux < quantidade; aux++) 
+   {
+       std::cout << msg_decodificada[aux]<<"\n" ;
+   }*/
+       
+   
+   
+   return msg_decodificada;
+}
